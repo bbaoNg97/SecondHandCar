3a5e08841093577769317c7bf6c5ed9091a9c835
bbaoNg97 <bbao0417@hotmail.com>
2018-10-07 16:46:11 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/secondhandcar/ResetPwActivity.java
@@ -0,0 +1,13 @@
+package my.edu.tarc.secondhandcar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResetPwActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pw);
+    }
+}
