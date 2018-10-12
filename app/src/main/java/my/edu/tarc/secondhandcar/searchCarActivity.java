51f78e4ca692253bca98c6b2063d33ee59ea6a47
bbaoNg97 <bbao0417@hotmail.com>
2018-10-12 21:39:54 +0800

    commit bottom nav bar

--- app/src/main/java/my/edu/tarc/secondhandcar/searchCarActivity.java
+++ app/src/main/java/my/edu/tarc/secondhandcar/searchCarActivity.java
@@ -3,6 +3,7 @@ package my.edu.tarc.secondhandcar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class searchCarActivity extends AppCompatActivity {
 
@@ -12,9 +13,20 @@ public class searchCarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_car);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.search_action_bar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
 
 
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
