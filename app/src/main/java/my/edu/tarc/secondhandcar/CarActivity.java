b96495a4da726b30a0c2db8682d3b33b582b170e
Alex <C_hua9495@hotmail.com>
2018-10-14 02:07:04 +0800

    car activity

--- app/src/main/java/my/edu/tarc/secondhandcar/CarActivity.java
+++ app/src/main/java/my/edu/tarc/secondhandcar/CarActivity.java
@@ -2,12 +2,21 @@ package my.edu.tarc.secondhandcar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 
 public class CarActivity extends Activity {
 
+    private ViewPager viewPager1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car);
+
+        viewPager1=(ViewPager)findViewById(R.id.viewPager1);
+
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(this);
+
+        viewPager1.setAdapter(viewPagerAdapter);
+
     }
 }
