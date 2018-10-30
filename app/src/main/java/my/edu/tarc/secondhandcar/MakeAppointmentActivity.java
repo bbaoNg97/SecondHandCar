9178266e5adf9003773e043d7437f7c37c96352b
bbaoNg97 <bbao0417@hotmail.com>
2018-10-30 11:34:47 +0800

    insert appointment,generate appID

--- app/src/main/java/my/edu/tarc/secondhandcar/MakeAppointmentActivity.java
+++ app/src/main/java/my/edu/tarc/secondhandcar/MakeAppointmentActivity.java
@@ -16,17 +16,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MakeAppointmentActivity extends AppCompatActivity {
 
@@ -34,11 +50,18 @@ public class MakeAppointmentActivity extends AppCompatActivity {
     private DatePickerDialog.OnDateSetListener mDateSetListener;
     private TimePickerDialog.OnTimeSetListener mTimeSetListener;
     private Button btnSendRequest;
+    private ProgressBar booking;
     private String time, custID;
     private Date selectedTime;
     SharedPreferences sharePref;
     RequestQueue queue;
     private String appID, carID;
+    RequestQueue requestQueue;
+    List<String> bookingList;
+    private int totalBooking;
+    public static final String TAG = "my.edu.tarc.secondhandcar";
+    String nextAppID;
+
 
 
     @Override
@@ -46,6 +69,7 @@ public class MakeAppointmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_appointment);
 
+        bookingList = new ArrayList<>();
         setTitle(R.string.title_chooseDateTime);
         if (!LoginActivity.isConnected(MakeAppointmentActivity.this)) {
             AlertDialog.Builder builder = new AlertDialog.Builder(MakeAppointmentActivity.this);
@@ -60,6 +84,8 @@ public class MakeAppointmentActivity extends AppCompatActivity {
         btnSendRequest = (Button) findViewById(R.id.buttonSendAppReq);
         textViewDate = (TextView) findViewById(R.id.textViewDate);
         textViewTime = (TextView) findViewById(R.id.textViewTime);
+        booking=(ProgressBar)findViewById(R.id.booking);
+        proceed();
         textViewDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -140,6 +166,8 @@ public class MakeAppointmentActivity extends AppCompatActivity {
             public void onClick(View view) {
                 try {
 
+                    getAllCustomer(MakeAppointmentActivity.this,getString(R.string.get_appointment_url));
+
                     final String strDate = textViewDate.getText().toString();
                     final String strTime = textViewTime.getText().toString();
 
@@ -202,11 +230,16 @@ public class MakeAppointmentActivity extends AppCompatActivity {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
 
+                                    booking.setVisibility(View.VISIBLE);
+                                    textViewDate.setEnabled(false);
+                                    textViewTime.setEnabled(false);
+                                    btnSendRequest.setEnabled(false);
+
                                     //Todo: store date, time, appID,custID,carID,status(pending by default)(no need) ,agentID(no need)
                                     //custID is get,date time also got(strDate, strTime)
                                     //Todo: get carID(using getString after done choosing car that part)
-                                    //Todo:generate appID
-                                    //makeServiceCall(MakeAppointmentActivity.this,getString(R.string.insert_booking_url),custID,carID,strDate,strTime);
+
+                                    makeServiceCall(MakeAppointmentActivity.this,getString(R.string.insert_booking_url),nextAppID,strDate,strTime,carID,custID);
 
 
 
@@ -240,6 +273,133 @@ public class MakeAppointmentActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void makeServiceCall(final Context context, String url, final String appID, final String appDate, final String appTime, final String carID, final String custID ) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        try {
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                String success = jsonObject.getString("success");
+                                String message = jsonObject.getString("message");
+                                //if register successful
+                                if (success.equals("1")) {
+                                    Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                                    //loading.setVisibility(View.GONE);
+                                    //btnSignUp.setEnabled(true);
+                                    //if success, go to login page
+                                    Intent mainIntent = new Intent(MakeAppointmentActivity.this, MainActivity.class);
+                                    startActivity(mainIntent);
+                                }
+
+                            } catch (JSONException e) {
+
+                                checkError(e, MakeAppointmentActivity.this);
+                                proceed();
+
+                            }
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            checkError(error, MakeAppointmentActivity.this);
+                            proceed();
+
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    //LHS is from php, RHS is getText there
+                    params.put("appID", appID);
+                    params.put("appDate", appDate);
+                    params.put("appTime", appTime);
+                    params.put("carID", carID);
+                    params.put("custID", custID);
+
+                    return params;
+                }
+            };
+
+
+            queue.add(stringRequest);
+        } catch (Exception e) {
+            checkError(e, MakeAppointmentActivity.this);
+            proceed();
+
+        }
+
+    }
+    //to count total number of appointment and count the total number of customer to generate ID
+    private void getAllCustomer(Context context, String url) {
+        // Instantiate the RequestQueue
+        requestQueue = Volley.newRequestQueue(context);
+
+
+
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(
+                url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            //everytime i listen to the server, i clear the list
+                            bookingList.clear();
+
+                            //if no appointment details
+                            if(response.length()==0){
+                                nextAppID = generateAppID(0);
+                            }
+                            else{
+                                //to generate next appointment ID
+                                nextAppID=generateAppID(totalBooking);
+                                for (int i = 0; i < response.length(); i++) {
+                                    JSONObject userResponse = (JSONObject) response.get(i);
+                                    //json object that contains all of the customer in the user table
+                                    String appID = userResponse.getString("appID");
+                                    bookingList.add(appID);
+                                    totalBooking++;
+                                }
+                            }
+
+
+
+                        } catch (Exception e) {
+
+                            checkError(e, MakeAppointmentActivity.this);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        checkError(volleyError, MakeAppointmentActivity.this);
+
+                    }
+                });
+
+        // Set the tag on the request.
+        jsonObjectRequest.setTag(TAG);
+
+        // Add the request to the RequestQueue.
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private void checkError(Exception e, Context context) {
+        if (!LoginActivity.isConnected(MakeAppointmentActivity.this)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MakeAppointmentActivity.this);
+            builder.setTitle("Connection Error");
+            builder.setMessage("No network.\nPlease try connect your network").setNegativeButton("Retry", null).create().show();
+
+        } else {
+            Toast.makeText(MakeAppointmentActivity.this, "Register failed! \n" + e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
     private String generateAppID(int countAppointment) {
         String id = "A";
 
@@ -262,5 +422,12 @@ public class MakeAppointmentActivity extends AppCompatActivity {
         }
 
         return id;
+    }
+
+    private void proceed(){
+        booking.setVisibility(View.GONE);
+        textViewDate.setEnabled(true);
+        textViewTime.setEnabled(true);
+        btnSendRequest.setEnabled(true);
     }
 }
